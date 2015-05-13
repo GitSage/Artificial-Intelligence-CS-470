693017322ec88de190a0359a28e0d4ce73dd900c
lexic92 <lexic92@gmail.com>
2015-05-12 19:44:42 -0600

    Added x-y coordinates that Ben told me to add. I have some questions about it being a accurate map representation.

--- AI/mapfields/four_ls.py
+++ AI/mapfields/four_ls.py
@@ -1,4 +1,6 @@
-import AI.PotentialFieldCalculator
+
+from __future__ import absolute_import
+from ..PotentialFieldCalculator import *
 
 
 __author__ = 'lexic92'
@@ -7,16 +9,43 @@ attractive = []
 repulsive = []
 tangential = []
 
-attractive.append(AttractiveObject(x=flag.x, y=flag.y, radius=10, spread=1000000, alpha=1))
-attractive.append(AttractiveObject(x=flag.x, y=flag.y, radius=10, spread=1000000, alpha=1))
-attractive.append(AttractiveObject(x=flag.x, y=flag.y, radius=10, spread=1000000, alpha=1))
-attractive.append(AttractiveObject(x=flag.x, y=flag.y, radius=10, spread=1000000, alpha=1))
-attractive.append(AttractiveObject(x=flag.x, y=flag.y, radius=10, spread=1000000, alpha=1))
-attractive.append(AttractiveObject(x=flag.x, y=flag.y, radius=10, spread=1000000, alpha=1))
+#Reference for syntax
+#attractive.append(AttractiveObject(x=150.0, y=120.0, radius=30, spread=1000000, alpha=1))
+
+#From the email list: (Treat the following string as a block comment)
+# Email list is missing the x=0, y=0 middle box thing.
+comment='''
+{'y': 120.0, 'x': 150.0}
+{'y': 180.0, 'x': 150.0}
+{'y': 120.0, 'x': 210.0}
+{'y': -120.0, 'x': 150.0}
+{'y': -120.0, 'x': 210.0}
+{'y': -180.0, 'x': 150.0}
+{'y': -120.0, 'x': -90.0}
+{'y': -180.0, 'x': -90.0}
+{'y': -120.0, 'x': -150.0}
+{'y': 120.0, 'x': -90.0}
+{'y': 180.0, 'x': -90.0}
+{'y': 120.0, 'x': -150.0}
+{'y': 0.0, 'x': 10.0}
 
 
-repulsive.append(RepulsiveObject(x=point[0], y=point[1], radius=3, spread=6, alpha=10))
-repulsive.append(RepulsiveObject(x=point[0], y=point[1], radius=3, spread=6, alpha=10))
-repulsive.append(RepulsiveObject(x=point[0], y=point[1], radius=3, spread=6, alpha=10))
-repulsive.append(RepulsiveObject(x=point[0], y=point[1], radius=3, spread=6, alpha=10))
-repulsive.append(RepulsiveObject(x=point[0], y=point[1], radius=3, spread=6, alpha=10))
+all 30 except the middle radius is 60.
+'''
+# I am just guessing on spread=100. Might need to adjust later.
+repulsive.append(RepulsiveObject(x=150.0, y=120.0, radius=30, spread=100, alpha=1))
+repulsive.append(RepulsiveObject(x=150.0, y=180.0, radius=30, spread=100, alpha=1))
+repulsive.append(RepulsiveObject(x=210.0, y=120.0, radius=30, spread=100, alpha=1))
+repulsive.append(RepulsiveObject(x=150.0, y=-120.0, radius=30, spread=100, alpha=1))
+repulsive.append(RepulsiveObject(x=210.0, y=-120.0, radius=30, spread=100, alpha=1))
+repulsive.append(RepulsiveObject(x=150.0, y=-180.0, radius=30, spread=100, alpha=1))
+repulsive.append(RepulsiveObject(x=-90.0, y=-120.0, radius=30, spread=100, alpha=1))
+repulsive.append(RepulsiveObject(x=-90.0, y=-180.0, radius=30, spread=100, alpha=1))
+repulsive.append(RepulsiveObject(x=-150.0, y=-120.0, radius=30, spread=100, alpha=1))
+repulsive.append(RepulsiveObject(x=-90.0, y=120.0, radius=30, spread=100, alpha=1))
+repulsive.append(RepulsiveObject(x=-90.0, y=180.0, radius=30, spread=100, alpha=1))
+repulsive.append(RepulsiveObject(x=-150.0, y=120.0, radius=30, spread=100, alpha=1))
+repulsive.append(RepulsiveObject(x=10.0, y=0.0, radius=30, spread=100, alpha=1))
+
+# Email list is missing the x=0, y=0 middle box thing. So I added it here:
+repulsive.append(RepulsiveObject(x=0.0, y=0.0, radius=60, spread=100, alpha=1))
