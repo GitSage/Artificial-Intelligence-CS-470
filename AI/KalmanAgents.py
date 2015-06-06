75626812dc8c7d172ae4f831d08e28077dcecac4
lajinnsolo <waerstein@gmail.com>
2015-06-06 17:38:16 -0600

    Improved the way that tanks choose a new goal

--- AI/KalmanAgents.py
+++ AI/KalmanAgents.py
@@ -1,14 +1,9 @@
 __author__ = 'ben'
 
-import OpenGL
-OpenGL.ERROR_CHECKING = False
-from OpenGL.GL import *
-from OpenGL.GLU import *
-from OpenGL.GLUT import *
-from numpy import zeros
-import numpy
-import time
 import Agents
+import math
+from Timer import Timer
+import numpy as npy
 
 class ClayPigeonAgent(Agents.Agent):
     pass
@@ -23,7 +18,72 @@ class NonConformingClayPigeon(ClayPigeonAgent):
     pass
 
 class KalmanAgent(Agents.Agent):
-    pass
+
+    def __init__(self, tank_index, state, pigeon_state):
+        self.state = state
+        self.pigeon_state = pigeon_state
+        self.tank_index = tank_index
+
+        self.xt = [self.state.mytanks[self.tank_index].x,  # x position
+                     0,  # x velocity
+                     0,  # x acceleration
+                     self.state.mytanks[self.tank_index].y,  # y position
+                     0,  # y velocity
+                     0]  # y acceleration
+
+        self.sigt = npy.array([[100.0, 0.0, 0.0, 0.0,   0.0, 0.0],  # initial covariance matrix. We don't think it's moving
+                          [0.0,   0.1, 0.0, 0.0,   0.0, 0.0],  # but we're uncertain where it is.
+                          [0.0,   0.0, 0.1, 0.0,   0.0, 0.0],
+                          [0.0,   0.0, 0.0, 100.0, 0.0, 0.0],
+                          [0.0,   0.0, 0.0, 0.0,   0.1, 0.0],
+                          [0.0,   0.0, 0.0, 0.0,   0.0, 0.1]])
+
+        dt = Timer.TIME_PER_TICK  # delta t
+        c = 0  # represents friction. The server has no friction, so this is usually 0.
+
+        self.F = npy.array([[1, dt, dt**2/2, 0, 0, 0],
+                       [0, 1,  dt,      0, 0, 0],
+                       [0, -c, 1,       0, 0, 0],
+                       [0, 0,  0,       1, dt, dt**2/2],
+                       [0, 0,  0,       0, 1, dt],
+                       [0, 0,  0,       0, -c, 1]])
+        self.F_trans = self.F.transpose()
+
+        self.H = npy.array([[1, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 1, 0, 0]])
+        self.H_trans = self.H.transpose()
+
+        self.sigx = npy.array([[0.1, 0.0, 0.0,   0.0,   0.0, 0.0],  # Variance of the predicted state noise. Constant.
+                          [0.0, 0.1, 0.0,   0.0,   0.0, 0.0],  #
+                          [0.0, 0.0, 100.0, 0.0,   0.0, 0.0],
+                          [0.0, 0.0, 0.0,   0.1,   0.0, 0.0],
+                          [0.0, 0.0, 0.0,   0.0,   0.1, 0.0],
+                          [0.0, 0.0, 0.0,   0.0,   0.0, 100.0]])
+
+        self.sigz = npy.array([[25, 0],  # this is the noise of the x and y locations
+                         [0, 25]])
+
+    def update(self):
+        FSFTsigx = self.F.dot(self.sig0).dot(self.F_trans) + self.sigx  # FΣtFT + Σx
+        HTBlahInverse = npy.inv(self.H.dot(FSFTsigx).dot(self.H_trans) + self.sigz)  # H(FΣtFT + Σx)HT + Σz)^−1
+        kalman_gain = FSFTsigx.dot(self.H_trans).dot(HTBlahInverse)  # Kt + 1 = (FΣtFT + Σx)HT(H(FΣtFT + Σx)HT + Σz)^−1
+
+
+        # self.xt = self.F.dot(self.xt + kalman_gain).dot
+
+
+    def dist(self, x, y):
+        return math.sqrt((x - self.x)**2 + (y - self.y)**2)
+
+    def ang(self, x, y):
+        angle = math.atan2(self.y - y, self.x - x)
+        '''Make any angle be between +/- pi.'''
+        angle -= 2 * math.pi * int(angle / (2 * math.pi))
+        if angle <= -math.pi:
+            angle += 2 * math.pi
+        elif angle > math.pi:
+            angle -= 2 * math.pi
+        return angle
 
 class KalmanVisualizer:
     pass
